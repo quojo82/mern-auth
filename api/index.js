e08eb34ca33e76d62b7cc3cdfeaa039e10e2a914
Jake Wood <quojo82@gmail.com>
2025-01-19 18:32:15 +0000

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from 'express'
 import { connectDB } from '../config/db.js'
+import userRoutes from './routes/user.route.js'
 
 const app =express()
+
+//test server
+
+/* app.get('/', (req, res)=>{
+    res.json({
+         message:'api is up and working'
+
+    }
+       
+    )
+})
+ */
+
+app.use('/api/user', userRoutes)
 
 app.listen(3000, ()=>{
    connectDB();
