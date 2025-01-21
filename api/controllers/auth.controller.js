eb1f04aca4e2938c607ab2f33056fca97371170a
Jake Wood <quojo82@gmail.com>
2025-01-21 17:44:19 +0000

    Add Error Handlers

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup =async(req, res)=>{
+export const signup =async(req, res, next)=>{
       const {username, email, password} =req.body;
       const hashedPassword =bcryptjs.hashSync(password, 10) 
       const newUSer = new User({username, email, password:hashedPassword})
@@ -10,10 +11,12 @@ export const signup =async(req, res)=>{
         await newUSer.save() 
         res.status(201).json({message: "User created successfully"})
       } catch(error){
-        res.status(500).json(
-          error.message
-        )
+        /*res.status(500).json({
+            message:"There is an Error"
+        }) */
 
+        //calling the error handler
+        next(error)
       }
       
 }
