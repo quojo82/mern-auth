27a6b3414ea57f33ca2befefec544ab00be2c36d
Jake Wood <quojo82@gmail.com>
2025-01-22 17:33:22 +0000

    complete signup page functionality

--- api/index.js
+++ api/index.js
@@ -1,29 +1,25 @@
-import express from 'express'
-import { connectDB } from '../config/db.js'
-import userRoutes from './routes/user.route.js'
-import authRoutes from './routes/auth.route.js'
+import express from "express";
+import { connectDB } from "../config/db.js";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
-const app =express()
-
+const app = express();
 
 app.use(express.json());
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
- app.use((err, req, res, next)=>{
-    const statusCode =err.statusCode || 500;
-    const message =err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success:false,
-        message,
-        statusCode
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error2";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
 
-    });
-
-}) 
-
-app.listen(3000, ()=>{
-   connectDB();
-    console.log('server up and running on port 3000!')
-})
-
+app.listen(3000, () => {
+  connectDB();
+  console.log("server up and running on port 3000!");
+});
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,39 +1,93 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const SignUp = () => {
-  return(
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  //handleChange
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  console.log(formData);
+
+  //handleSubmit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch(
+        "/api/auth/signup",
+
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      //convert res to data
+      const data = await res.json();
+      //console.log(data);
+      setLoading(false);
+
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  return (
     <div className="p-3 max-w-lg mx-auto">
-    <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-    <form className="flex flex-col gap-4">
-      <input
-        type="text"
-        placeholder="Username"
-        id="username"
-        className="bg-slate-100 p-3 rounded-lg"
-      />
-      <input
-        type="text"
-        placeholder="Email"
-        id="username"
-        className="bg-slate-100 p-3 rounded-lg"
-      />
-      <input
-        type="text"
-        placeholder="Password"
-        id="username"
-        className="bg-slate-100 p-3 rounded-lg"
-      />
-   <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80" >Sign Up</button>
-    </form>
-    <div className="flex gap-2 mt-3">
-      <p>Have an account?</p>
-      <Link to ='/signin'><span class='text-blue-500'>Sign in</span></Link>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign Up"}
+        </button>
+      </form>
+
+      <div className="flex gap-2 mt-3">
+        <p>Have an account?</p>
+        <Link to="/signin">
+          <span class="text-blue-500">Sign in</span>
+        </Link>
+      </div>
+      <p className="text-red-700 mt-5">{error && "Something Went Wrong"}</p>
     </div>
-  </div>
-
-  )
-
+  );
 };
 
 export default SignUp;
