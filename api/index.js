8c50c464c929ccdc867d51131bc7e31223ba1e62
Jake Wood <quojo82@gmail.com>
2025-02-20 20:47:25 +0000

    create apdate user api route

--- api/index.js
+++ api/index.js
@@ -2,10 +2,14 @@ import express from "express";
 import { connectDB } from "../config/db.js";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
