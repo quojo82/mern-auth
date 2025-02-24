1b7a84f1f2c3bb293f5d7d1e0a19815d3477695c
Jake Wood <quojo82@gmail.com>
2025-02-24 14:42:01 +0000

    add pre production changes

--- api/index.js
+++ api/index.js
@@ -3,15 +3,24 @@ import { connectDB } from "../config/db.js";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import path from 'path';
+
+const __dirname =path.resolve()
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'client/dist'))) 
 
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
