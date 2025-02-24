ead9da0599a092ed553ba437c11785d12cf17aa3
Jake Wood <quojo82@gmail.com>
2025-02-24 12:32:06 +0000

    complete home and about pages

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
        <Link to='/'> <h1 className='font-bold'>Auth App</h1></Link>
         <ul className='flex gap-4'>
             <Link to ='/'><li>Home</li></Link>
-            <Link to ='/contact'><li>Contact</li></Link>
+            <Link to ='/about'><li>About</li></Link>
             <Link to='/profile'>
             {
               currentUser ? (
